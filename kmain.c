22aa3eea22b4f0b92523d514de27fd449ddc9aff
krishanshamod <krishanshamod@gmail.com>
2021-08-01 02:47:13 +0530

    Moving the Cursor

--- kmain.c
+++ kmain.c
@@ -1,3 +1,11 @@
+    #include "io.h"
+
+    #define FB_COMMAND_PORT         0x3D4
+    #define FB_DATA_PORT            0x3D5
+
+    #define FB_HIGH_BYTE_COMMAND    14
+    #define FB_LOW_BYTE_COMMAND     15
+
     #define FB_GREEN     2
     #define FB_DARK_GREY 8
 
@@ -19,8 +27,25 @@
         fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
     }
 
+
+    /** fb_move_cursor:
+     *  Moves the cursor of the framebuffer to the given position
+     *
+     *  @param pos The new position of the cursor
+     */
+
+    void fb_move_cursor(unsigned short pos)
+    {
+        outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
+        outb(FB_DATA_PORT,    ((pos >> 8) & 0x00FF));
+        outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
+        outb(FB_DATA_PORT,    pos & 0x00FF);
+    }
+
+
     void kmain()
     {
-
-        fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
+       // fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
+       fb_move_cursor(00);
     }
+
