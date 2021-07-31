437f474afb35530dcf5d5a5b73f9d07cd5067a6f
krishanshamod <krishanshamod@gmail.com>
2021-08-01 02:06:07 +0530

    Working with Framebuffer

--- kmain.c
+++ kmain.c
@@ -1,5 +1,26 @@
-    /* The C function */
-    int sum_of_three(int arg1, int arg2, int arg3)
+    #define FB_GREEN     2
+    #define FB_DARK_GREY 8
+
+    char *fb = (char *) 0x000B8000;
+
+    /** fb_write_cell:
+     *  Writes a character with the given foreground and background to position i
+     *  in the framebuffer.
+     *
+     *  @param i  The location in the framebuffer
+     *  @param c  The character
+     *  @param fg The foreground color
+     *  @param bg The background color
+     */
+
+    void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
     {
-        return arg1 + arg2 + arg3;
+        fb[i] = c;
+        fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
+    }
+
+    void kmain()
+    {
+
+        fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
     }
--- loader.s
+++ loader.s
@@ -30,12 +30,12 @@
 
 
     ; The assembly code
-    extern sum_of_three   ; the function sum_of_three is defined elsewhere
+    extern kmain   ; the function sum_of_three is defined elsewhere
 
     push dword 3            ; arg3
     push dword 2            ; arg2
     push dword 1            ; arg1
-    call sum_of_three       ; call the function, the result will be in eax
+    call kmain              ; call the function, the result will be in eax
 
 
     .loop:
