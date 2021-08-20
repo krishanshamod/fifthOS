55e6d2f5f663b5234a034707318682e5878b516b
krishanshamod <krishanshamod@gmail.com>
2021-08-20 14:39:41 +0530

    Called necessary functions in kmain.c

--- kmain.c
+++ kmain.c
@@ -1,15 +1,21 @@
     #include "drivers/frame_buffer.h"
     #include "drivers/serial_port.h"
+    #include "drivers/io.h"
     #include "segmentation/segments.h"
+    #include "interrupts/interrupts.h"
+    #include "interrupts/keyboard.h"
+    #include "interrupts/pic.h"
+    
 
     void kmain()
     {
 
-       char arr[] = "Welcome to fifthOS";
-       fb_write(arr, 20);
-       serial_configure(SERIAL_COM1_BASE, Baud_115200);
-       serial_write(SERIAL_COM1_BASE, arr, 20);
+       //char arr[] = "Welcome to fifthOS";
+       //fb_move_cursor(6*80);
+       //fb_write(arr, 20);
+       //serial_write(arr, 20);
        segments_install_gdt();
+       interrupts_install_idt();
 
     }
 
