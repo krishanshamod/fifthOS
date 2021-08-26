9ca264d22c3ef7fe0cf321ee6a9cd9f109d13345
krishanshamod <krishanshamod@gmail.com>
2021-08-27 03:02:46 +0530

    Updated the kmain.c file to call our simple program

--- kmain.c
+++ kmain.c
@@ -5,17 +5,37 @@
     #include "interrupts/interrupts.h"
     #include "interrupts/keyboard.h"
     #include "interrupts/pic.h"
+    #include "multiboot.h"
     
 
-    void kmain()
+    int kmain(unsigned int ebx)
     {
 
-       //char arr[] = "Welcome to fifthOS";
-       //fb_move_cursor(6*80);
-       //fb_write(arr, 20);
-       //serial_write(arr, 20);
-       segments_install_gdt();
-       interrupts_install_idt();
+      //char arr[] = "Welcome to fifthOS";
+      //fb_move_cursor(6*80);
+      //fb_write(arr, 20);
+      //serial_write(arr, 20);
+      segments_install_gdt();
+      interrupts_install_idt();
 
+      multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
+      multiboot_module_t* modules = (multiboot_module_t*) mbinfo->mods_addr; 
+      unsigned int address_of_module = modules->mod_start;
+
+      if((mbinfo->mods_count) == 1){
+         char str[] = "Module successfully loaded";
+         serial_write(str,sizeof(str));
+         
+         typedef void (*call_module_t)(void);
+            call_module_t start_program = (call_module_t) address_of_module;
+            start_program();
+
+      }
+      else{
+         char str[] = "Multiple modules loaded";
+         serial_write(str,sizeof(str));
+      }
+
+      return 0;
     }
 
