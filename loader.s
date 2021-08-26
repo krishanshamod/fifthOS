9a585e562488904893bc52e997f56227b86eaf60
krishanshamod <krishanshamod@gmail.com>
2021-08-27 02:53:31 +0530

    Updated the loader.s file with module loading instructions

--- loader.s
+++ loader.s
@@ -5,10 +5,10 @@ extern kmain
 KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
 
 MAGIC_NUMBER    equ 0x1BADB002     ; define the magic number constant
-FLAGS           equ 0x00000001     ; 1 : tell GRUB to align modules
+ALIGN_MODULES   equ 0x00000001      ; tell GRUB to align modules
 
-CHECKSUM        equ -(MAGIC_NUMBER + FLAGS ) ; calculate the checksum (all options + checksum should equal 0)
-
+; calculate the checksum (all options + checksum should equal 0)
+CHECKSUM        equ -(MAGIC_NUMBER + ALIGN_MODULES)
 
 section .bss
 align 4                         ; align at 4 bytes
@@ -17,9 +17,9 @@ kernel_stack:                   ; label points to beginning of memory
 
 section .text                   ; start of the text (code) section
 align 4                         ; the code must be 4 byte aligned
-    dd MAGIC_NUMBER             ; write the magic number to the machine code,
-    dd FLAGS                    ; the flags,
-    dd CHECKSUM                 ; and the checksum
+    dd MAGIC_NUMBER                 ; write the magic number
+    dd ALIGN_MODULES                ; write the align modules instruction
+    dd CHECKSUM                     ; write the checksum
 
 mov esp, kernel_stack + KERNEL_STACK_SIZE       ; point esp to the start of the
                                                 ; stack (end of memory area)
